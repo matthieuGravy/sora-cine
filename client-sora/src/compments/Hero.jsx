64c22d2a3a0ed784d0249619c15e4d897543f852
iota07 <145263906+iota07@users.noreply.github.com>
2023-11-29 10:16:15 +0100

    adding title and description props to Hero component

--- client-sora/src/compments/Hero.jsx
+++ client-sora/src/compments/Hero.jsx
@@ -3,27 +3,25 @@ import bg from "../assets/images/Background03.jpg"
 import WatchNowButton from './WatchNowButton'
 
 
-function Hero() {
-
+function Hero({ title = 'Default Title', description = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt error id vitae consequuntur fugit repudiandae quaerat, dolores provident, aliquid, non quis et pariatur velit neque molestias? Harum asperiores at totam.' }) {
   const style = {
     backgroundImage: `url(${bg})`,
-    backgroundSizer: "cover",
+    backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundPosition: "center",
-  }
+  };
 
   return (
     <section className='h-screen w-screen' style={style}>
-      <div className="text-[#0D0630] h-full w-3/12 flex flex-col justify-center px-[5rem]">       
-        <h1 className="text-4xl font-bold mb-4">H1 title</h1> {/* insert component H1*/}
-        <p className="text-lg mb-8"><h3>Anime synopsis</h3> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt quis corporis eaque vero, tempore eum inventore saepe quidem architecto nihil vitae in tempora distinctio officiis vel repellendus quisquam aspernatur sunt.
-        </p>          
-        <p><WatchNowButton/></p>        
-        
+      <div className="text-[#0D0630] h-full w-3/12 flex flex-col justify-center px-[5rem]">
+        <h1 className="text-4xl font-bold mb-4">{title}</h1>
+        <p className="text-lg mb-8">
+          <h2 className='font-bold'>synopsis</h2> {description}
+        </p>
+        <p><WatchNowButton/></p>
       </div>
-
     </section>
-  )
+  );
 }
 
 export { Hero }
