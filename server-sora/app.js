97e467d285450ead1c49d83cfa2f4b365f304ef2
Matthieu Gravy <86630163+matthieuGravy@users.noreply.github.com>
2023-12-04 13:17:51 +0100

    Update app.js

--- server-sora/app.js
+++ server-sora/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
+
 require("dotenv").config();
 
-const app = express();
 app.use(express.json());
 const port = 3200;
 
