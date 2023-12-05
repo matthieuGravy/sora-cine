9b0d42c42c64beda1b561937165d5ac6a7d78faf
matthieuGravy <matthieugravy@gmail.com>
2023-12-05 16:28:27 +0100

    buttons modal

---
+++ client-sora/src/compments/Buttons.jsx
@@ -0,0 +1,23 @@
+const ButtonPrimary = (props) => {
+  return (
+    <button
+      className="bg-slate-200 text-slate-800 rounded-full px-4 py-2"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+const ButtonSecondary = (props) => {
+  return (
+    <button
+      className="bg-slate-200 text-slate-800 rounded-full px-4 py-2"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export { ButtonPrimary, ButtonSecondary };
