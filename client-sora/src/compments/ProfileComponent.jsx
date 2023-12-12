9e34f7228f99183a025ba648b1438c971172fab8
iota07 <hanesananhouvannasak@googlemail.com>
2023-12-12 16:54:53 +0100

    adding PUT request functionality on profile-page

--- client-sora/src/compments/ProfileComponent.jsx
+++ client-sora/src/compments/ProfileComponent.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import profile_icon from "../assets/images/Naruto-PNG-Clipart.png";
 
 function ProfileComponent() {
+  const [user, setUser] = useState({
+    _id: "",
+    firstname: "",
+    lastname: "",
+    birthday: "",
+    email: "",
+    password: "",
+  });
+
+  // Maintain a state for each form field to track edit mode
+  const [editModes, setEditModes] = useState({
+    firstname: false,
+    lastname: false,
+    birthday: false,
+    email: false,
+    password: false,
+  });
+
+  useEffect(() => {
+    // Fetch user data from the server when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:3200/user/65786d43bc878cd0d66e1dae"
+        );
+        const userData = await response.json();
+        setUser(userData.message); // Use userData.message to get the user object
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const formattedBirthday = user.birthday
+    ? new Date(user.birthday).toISOString().split("T")[0]
+    : "Loading...";
+
+  // Function to handle edit button click
+  const handleEditClick = (fieldName) => {
+    setEditModes((prevEditModes) => ({
+      ...prevEditModes,
+      [fieldName]: !prevEditModes[fieldName], // Toggle edit mode
+    }));
+  };
+
+  // Function to handle save button click
+  const handleSaveClick = (fieldName) => {
+    // Perform the logic to save the data to the database
+    // (You need to implement this part)
+
+    // After saving, toggle back to non-edit mode
+    setEditModes((prevEditModes) => ({
+      ...prevEditModes,
+      [fieldName]: false,
+    }));
+  };
+
+  const handleEditButton = (e, fieldName) => {
+    e.preventDefault(); // Prevent page reload
+
+    handleEditClick(fieldName);
+  };
+
   return (
     <>
       <section className="grid grid-cols-6 min-w-fit max-w-md sm:max-w-lg border bg-white px-6 py-14 shadow-md rounded-2xl items-center justify-between">
@@ -40,7 +105,7 @@ function ProfileComponent() {
           </svg>
         </figure>
         <div className="text-2xl col-span-4 pt-16 font-bold">
-          Uzumaki Naruto
+          {user.lastname} {user.firstname}
         </div>
         <form className="col-span-6 col-start-1 grid grid-cols-4 justify-between">
           <p className="pt-6 col-span-2">Name</p>
@@ -48,12 +113,29 @@ function ProfileComponent() {
             id="firstname"
             type="text"
             name="firstname"
-            className="w-full text-md font-bold col-start-1 col-span-3 placeholder:text-black placeholder:font-bold"
-            placeholder="Naruto"
+            className={`w-full text-md font-bold col-start-1 col-span-3 placeholder:text-black placeholder:font-bold ${
+              editModes.firstname ? "bg-slate-100" : ""
+            } ${
+              !editModes.firstname ? "border-none" : ""
+            } focus:border-none focus:outline-none`}
+            placeholder={user.firstname}
+            readOnly={!editModes.firstname}
           />
-          <button className="border-2 rounded-lg bg-gray-400 text-white px-2 py-1 col-start-6">
-            Edit
-          </button>
+          {editModes.firstname ? (
+            <button
+              className="border-2 rounded-lg bg-blue-400 text-white px-2 py-1 col-start-6"
+              onClick={(e) => handleSaveClick("firstname", e)}
+            >
+              Save
+            </button>
+          ) : (
+            <button
+              className="border-2 rounded-lg bg-gray-400 text-white px-2 py-1 col-start-6"
+              onClick={(e) => handleEditButton(e, "firstname")}
+            >
+              Edit
+            </button>
+          )}
 
           <p className="pt-6 col-span-2">Surname</p>
           <input
@@ -61,19 +143,19 @@ function ProfileComponent() {
             type="text"
             name="lastname"
             className="w-full text-md font-bold col-start-1 col-span-3 placeholder:text-black placeholder:font-bold"
-            placeholder="Uzumaki"
+            placeholder={user.lastname}
           />
           <button className="border-2 rounded-lg bg-gray-400 text-white px-2 py-1 col-start-6">
             Edit
           </button>
 
-          <p className="pt-6 col-span-2">Birthdate</p>
+          <p className="pt-6 col-span-2">Date of birth</p>
           <input
             id="birthday"
             type="text"
             name="birthday"
             className="w-full text-md font-bold col-start-1 col-span-3 placeholder:text-black placeholder:font-bold"
-            placeholder="October 10th"
+            placeholder={formattedBirthday}
           />
           <button className="border-2 rounded-lg bg-gray-400 text-white px-2 py-1 col-start-6">
             Edit
@@ -85,7 +167,7 @@ function ProfileComponent() {
             type="text"
             name="email"
             className="w-full text-md font-bold col-start-1 col-span-3 placeholder:text-black placeholder:font-bold"
-            placeholder="Naruto@Hokage.konoha"
+            placeholder={user.email}
           />
           <button className="border-2 rounded-lg bg-gray-400 text-white px-2 py-1 col-start-6">
             Edit
@@ -100,7 +182,7 @@ function ProfileComponent() {
             placeholder="********"
           />
           <button className="border-2 rounded-lg bg-gray-400 text-white px-2 py-1 col-start-5 col-span-2">
-            Change
+            Edit
           </button>
         </form>
         <button className="border-2 border-red-500 rounded-lg col-span-6 p-5 mt-8 text-red-500 font-bold">
