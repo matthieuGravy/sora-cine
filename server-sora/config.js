85ff47e3ceafed923a5e9e45a5888fbf3dc2e124
Matthieu Gravy <86630163+matthieuGravy@users.noreply.github.com>
2023-12-12 16:23:31 +0100

    Update config.js 2

--- server-sora/config.js
+++ server-sora/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://burak:burakbecode@cluster0.8gjkupo.mongodb.net/soracine",
-
-  tmdbApiKey: "c8e37a819c856aff51d6efd1114d3534",
+    process.env.MONGO_URI,
+  tmdbApiKey :
+    process.env.tmdbApiKey,
+  
 };
+
