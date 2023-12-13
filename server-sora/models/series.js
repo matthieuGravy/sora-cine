68e3cebdcd9f44ba9aadfaaf2fc5fea6af800455
Jessy Tibou <tibou.jessy@outlook.fr>
2023-12-13 15:43:03 +0100

    add-deleteusers for han and api get

---
+++ server-sora/models/series.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const seriesSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    overview: {
+      type: String,
+      required: true,
+    },
+    poster_path: {
+      type: String,
+      required: true,
+    },
+    backdrop_path: {
+      type: String,
+      required: true,
+    },
+    vote_average: {
+      type: Number,
+      required: true,
+    },
+    first_air_date: {
+      type: Date,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { collection: "series" }
+)
+
+const seriesModel = mongoose.model("Series", seriesSchema)
+
+module.exports = seriesModel
