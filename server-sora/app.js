a678e67bcf68a96b0038767541ceb4a10819d421
Jessy Tibou <tibou.jessy@outlook.fr>
2023-12-14 10:00:40 +0100

    fix get, get by id and finish factorisation

--- server-sora/app.js
+++ server-sora/app.js
@@ -1,149 +1,84 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
-const cors = require("cors")
-const seriesModel = require('./models/series');
-const { getSeries, getSeriesById, PostSeries } = require("./routes/series");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const seriesModel = require("./models/series");
+const { getSeries, getSeriesById } = require("./routes/series");
 const { getAnimeData } = require("./controllers/api");
-
+const { getContact, postContact } = require("./routes/constact");
 const {
   getAllUsers,
   getUserById,
   postUser,
   deleteUserById,
   putUserById,
-} = require("./routes/users")
-const { getLogin, postLogin } = require("./routes/login")
-require("dotenv").config()
+} = require("./routes/users");
+const { postDeletedUsers } = require("./routes/deletedUsers");
+const { getLogin, postLogin } = require("./routes/login");
+require("dotenv").config();
 
-app.use(express.json())
-const port = 3200
+app.use(express.json());
+const port = 3200;
 
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
 function connectDB() {
   mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  const db = mongoose.connection
-  db.on("error", (err) => {
-    console.error("MongoDB connection error:", err)
-  })
+  });
+  const db = mongoose.connection;
+  db.on("error", err => {
+    console.error("MongoDB connection error:", err);
+  });
   db.once("open", () => {
-    console.log("MongoDB connected successfully")
-  })
+    console.log("MongoDB connected successfully");
+  });
   db.on("disconnected", () => {
-    console.log("MongoDB disconnected")
-  })
+    console.log("MongoDB disconnected");
+  });
   process.on("SIGINT", () => {
     db.close(() => {
-      console.log("MongoDB connection closed")
-      process.exit(0)
-    })
-  })
+      console.log("MongoDB connection closed");
+      process.exit(0);
+    });
+  });
 }
 
 app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
 
 //users
 
-app.get("/user", getAllUsers)
-app.get("/user/:id", getUserById)
-app.post("/user", postUser)
-app.delete("/user/:id", deleteUserById)
-app.put("/user/:id", putUserById)
+app.get("/user", getAllUsers);
+app.get("/user/:id", getUserById);
+app.post("/user", postUser);
+app.delete("/user/:id", deleteUserById);
+app.put("/user/:id", putUserById);
 
 //deletedUsers
 
-//deletedUsers/post
-
-app.post("/deletedUsers", async (req, res) => {
-  try {
-    const deletedUsers = await userModel.find()
-    res.json({ success: true, message: deletedUsers })
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message })
-  }
-})
+app.post("/deletedUsers", postDeletedUsers);
 
 //login
 
-app.post("/login", postLogin)
-app.get("/login", getLogin)
+app.post("/login", postLogin);
+app.get("/login", getLogin);
 
 //series
 
-app.get("/series", getSeries)
-app.get("/series/:id", getSeriesById)
-app.post("/series", PostSeries)
+app.get("/series", getSeries);
+app.get("/series/:id", getSeriesById);
 
-//contact form
+//Contact
 
-//contactSchema
-
-const contactSchema = new mongoose.Schema(
-  {
-    lastname: {
-      type: String,
-      required: true,
-    },
-    firstname: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    subject: {
-      type: String,
-    },
-    content: {
-      type: String,
-    },
-    created_at: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  { collection: "contacts" }
-)
-
-const contactModel = mongoose.model("Contact", contactSchema)
-
-//contact/post
-
-app.post("/contact", async (req, res) => {
-  try {
-    const contact = new contactModel(req.body)
-    const savedContact = await contact.save()
-    res.json({ success: true, message: savedContact })
-  } catch (error) {
-    if (error.name === "ValidationError") {
-      res.status(400).json({ success: false, message: error.message })
-    } else {
-      res.status(500).json({ success: false, message: error.message })
-    }
-  }
-})
-
-//contact/get
-
-app.get("/contact", async (req, res) => {
-  try {
-    const contact = await contactModel.find()
-    res.json({ success: true, message: contact })
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message })
-  }
-})
+app.get("/contact", getContact);
+app.post("/contact", postContact);
 
 async function saveSeriesToDatabase() {
   const seriesData = await getAnimeData();
@@ -170,21 +105,23 @@ async function saveSeriesToDatabase() {
       await seriesInstance.save();
       console.log(`Série enregistrée dans la base de données: ${serie.name}`);
     } catch (error) {
-      console.error(`Erreur lors de l'enregistrement de la série ${serie.name}:`, error);
+      console.error(
+        `Erreur lors de l'enregistrement de la série ${serie.name}:`,
+        error
+      );
       // Gérer l'erreur selon vos besoins
     }
   }
 }
 
-
-saveSeriesToDatabase()
+saveSeriesToDatabase();
 
 mongoose.connection.on("connected", (err, res) => {
-  console.log("mongoose is connected")
-})
+  console.log("mongoose is connected");
+});
 
-connectDB()
+connectDB();
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- server-sora/routes/series.js
+++ server-sora/routes/series.js
@@ -1,55 +1,23 @@
-const mongoose = require("mongoose")
-const { fetchTrendingTvShows } = require("../controllers/api")
-const seriesModel = require("../models/series")
+const seriesModel = require("../models/series");
 
 //get animation series
 
 exports.getSeries = async (req, res) => {
   try {
-    const series = await fetchTrendingTvShows()
-    res.json({ success: true, message: series })
+    const series = await seriesModel.find();
+    res.json({ success: true, message: series });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message })
+    res.status(500).json({ success: false, message: error.message });
   }
-}
+};
 
 //get animation series by id
 
 exports.getSeriesById = async (req, res) => {
   try {
-    const id = req.params.id
-    console.log("ID:", id) // Ajoutez ceci pour afficher l'ID dans la console
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      console.log("Invalid ObjectId") // Ajoutez ceci pour déboguer
-      return res
-        .status(400)
-        .json({ success: false, message: "Invalid ObjectId" })
-    }
-
-    const series = await fetchTrendingTvShows.findById(id)
-
-    if (!series) {
-      console.log("Series not found") // Ajoutez ceci pour déboguer
-      return res
-        .status(404)
-        .json({ success: false, message: "Series not found" })
-    }
-
-    res.json({ success: true, message: series })
+    const series = await seriesModel.findById(req.params.id);
+    res.json({ success: true, message: series });
   } catch (error) {
-    console.error("Error:", error) // Ajoutez ceci pour déboguer
-    res.status(500).json({ success: false, message: error.message })
+    res.status(500).json({ success: false, message: error.message });
   }
-}
-
-//post animation series
-
-exports.PostSeries = async (req, res) => {
-  try {
-    const series = await seriesModel.create(req.body)
-    res.json({ success: true, message: series })
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message })
-  }
-}
+};
